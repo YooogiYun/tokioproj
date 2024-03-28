6a011f870903d95d29664b7250882bdbb6706787
unic <qq643374843@163.com>
2024-03-29 01:04:12 +0800

    channel almost done.

---
+++ src/bin/client.rs
@@ -0,0 +1,122 @@
+use bytes::Bytes;
+use mini_redis::client;
+use tokio::{sync::mpsc, sync::oneshot};
+
+// provided by the requester and used bt the manager task to send
+// the command response back to the requester
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
+
+#[derive(Debug)]
+enum Command {
+    GET {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    SET {
+        key: String,
+        value: Bytes,
+        resp: Responder<()>,
+    },
+}
+
+#[tokio::main]
+async fn main() {
+    // create a new channel with capacity of at most 32.
+    println!("start of client");
+    let (tx, mut rx) = mpsc::channel(32);
+
+    let tx2 = tx.clone();
+
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        println!("start of manager");
+
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+
+            match cmd {
+                GET { key, resp } => {
+                    println!("Cmd is GET, key is {}", &key);
+                    let res = client.get(&key).await;
+                    let _ = resp.send(res);
+                }
+                SET { key, value, resp } => {
+                    println!("Cmd is SET, key is {},value is {:?}", &key, &value);
+                    let res = client.set(&key, value).await;
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    });
+
+    // Spawn two tasks, one setting a value and other querying for key that was
+    // set.
+    let t1 = tokio::spawn(async move {
+        // tx.send("Sending from first handle").await.unwrap();
+        println!("Start of task 1");
+
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::GET {
+            key: "foo".into(),
+            resp: resp_tx,
+        };
+
+        // send the GET request
+        tx.send(cmd).await.unwrap();
+        println!("Task 1: Send done.");
+
+        // Await the response
+        let resp = resp_rx.await;
+        println!("Task 1: GOT = {:?}", resp);
+    });
+
+    let t2 = tokio::spawn(async move {
+        // tx2.send("Sending from scecond handle").await.unwrap();
+        println!("Start of task 2");
+
+        let (resp_tx, resp_rx) = oneshot::channel();
+
+        let cmd = Command::SET {
+            key: "foo".into(),
+            value: "bar".into(),
+            resp: resp_tx,
+        };
+
+        // send the SET request
+        tx2.send(cmd).await.unwrap();
+        println!("Task 2: Send done.");
+
+        // Await the response
+        let resp = resp_rx.await;
+        println!("Task 2: GOT = {:?}", resp);
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+
+    manager.await.unwrap();
+
+    // while let Some(msg) = rx.recv().await {
+    //     println!("GOT: {}", msg);
+    // }
+
+    // ... rest comes here
+}
+
+// #[tokio::main]
+// async fn main() {
+//     // estabish a connection to the server
+//     let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+//     // spawn two tasks, one gets a key, the other sets a key
+//     let t1 = tokio::spawn(async {
+//         let res = client.get("foo").await;
+//     });
+
+//     let t2 = tokio::spawn(async {
+//         client.set("foo", "bar".into()).await;
+//     });
+
+//     t1.await.unwrap();
+//     t2.await.unwrap();
+// }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,34 +1,56 @@
+use core::num;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+type ShardedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+
+        let db = db.clone();
+
         // process(socket).await; // it's only for one socket at same time.
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
+fn get_sharded_db(num_shard: usize) -> ShardedDb {
+    let mut sharded_db = Vec::with_capacity(num_shard);
+    for _ in 0..num_shard {
+        sharded_db.push(Mutex::new(HashMap::new()));
+    }
+    Arc::new(sharded_db)
+}
 
-    let mut db = HashMap::new();
+async fn process(socket: TcpStream, db: Db) {
+    use mini_redis::Command::{self, Get, Set};
 
     let mut connection = Connection::new(socket);
 
     while let Some(cmd) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(cmd).unwrap() {
             Set(data) => {
-                db.insert(data.key().to_string(), data.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(data.key().to_string(), data.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(data) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(data.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
