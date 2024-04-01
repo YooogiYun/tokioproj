7367085084ba685fab5913204079f83f85e045e2
unic <qq643374843@163.com>
2024-04-02 00:14:26 +0800

    🧷fix the error about stream ending

--- src/bin/echo-client-copy.rs
+++ src/bin/echo-client-copy.rs
@@ -1,5 +1,8 @@
 use std::fmt::format;
+use std::thread::sleep;
+use std::time::Duration;
 
+use mini_redis::client::Client;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 
 use tokio::net::TcpStream;
@@ -10,22 +13,33 @@ const MAX_TIMES: u8 = 12;
 // client.rs
 async fn main() -> io::Result<()> {
     let mut client = TcpStream::connect("127.0.0.1:6142").await?;
+    let mut client2 = TcpStream::connect("127.0.0.1:6142").await?;
 
-    let times = write_data(&mut client, MAX_TIMES).await?;
+    let task1 = tokio::spawn(async move {
+        println!("task1");
+        let times = write_data(&mut client, MAX_TIMES - 10, "1").await;
+    });
 
+    let task2 = tokio::spawn(async move {
+        println!("task2");
+        // sleep(Duration::from_secs(4));
+        let times = write_data(&mut client2, MAX_TIMES - 10, "2").await;
+    });
     // reconnect when the connection is closed even it not reach the MAX_TIMES
-    if times < MAX_TIMES {
-        write_data(&mut client, MAX_TIMES - times).await?;
-    }
+    // if times < MAX_TIMES {
+    //     write_data(&mut client, MAX_TIMES - times).await?;
+    // }
+
+    let result = tokio::join!(task1, task2);
 
     Ok(())
 }
 
-async fn write_data(client: &mut TcpStream, times: u8) -> io::Result<u8> {
+async fn write_data(client: &mut TcpStream, times: u8, name: &str) -> io::Result<u8> {
     let mut x = 0;
 
     while x < times {
-        let data = format!("No{}. Data from cilent: {}", x, "Hello");
+        let data = format!("No{}. Data from cilent {}: {}", x, name, "Hello");
         // println!("data len: {}", &data.as_bytes().len());
         client.write(data.as_bytes()).await?;
         x = x + 1;
@@ -33,7 +47,7 @@ async fn write_data(client: &mut TcpStream, times: u8) -> io::Result<u8> {
         // try to read the response from server
         let mut buffer = [0; 1024];
         match client.read(&mut buffer).await {
-            Ok(n) if n == 0 => {
+            Ok(0) => {
                 // if the count of bytes is 0, the connection is closed
                 eprintln!("Connection closed by peer");
                 break;
--- src/bin/echo-server-copy.rs
+++ src/bin/echo-server-copy.rs
@@ -18,19 +18,28 @@ async fn main() -> io::Result<()> {
 
             loop {
                 let mut buffer = vec![0; 1024];
-                if rd.read(&mut buffer).await.is_err() {
-                    eprintln!("Fail to read value");
-                    return;
-                };
-                let str_value = match String::from_utf8(buffer) {
-                    Ok(val) => val,
-                    Err(e) => {
-                        eprintln!("Error on parsing value: {}", e);
+                let n = match rd.read(&mut buffer).await {
+                    Ok(0) => {
+                        eprintln!("End of reading value.");
+                        return;
+                    }
+                    Ok(n) => n,
+                    Err(_) => {
+                        eprintln!("Fail to read value");
                         return;
                     }
                 };
-                println!("Server GOT from reader: {}", str_value);
+
+                let str_value = String::from_utf8_lossy(&buffer[0..n]);
+
+                println!(
+                    "Server GOT from reader: {}, len is {:?}",
+                    str_value,
+                    &str_value.get(0..4)
+                );
+
                 let mut reader = BufReader::new(str_value.as_bytes());
+
                 if io::copy(&mut reader, &mut wr).await.is_err() {
                     eprintln!("Fail to echo value");
                     return;
@@ -42,25 +51,38 @@ async fn main() -> io::Result<()> {
         //     let mut buf = vec![0; 1024];
         //     loop {
         //         let reader = socket.read(&mut buf).await;
-        //         println!(
-        //             "Reader read done. The value in the buffer is: {:?}",
-        //             String::from_utf8_lossy(&mut buf[..])
-        //         );
+        //         // println!(
+        //         //     "Reader read done. The value in the buffer is: {:?}",
+        //         //     String::from_utf8_lossy(&mut buf[0..40])
+        //         // );
+        //         println!("loop");
         //         match reader {
         //             // Return value of `Ok(0)` signifies that the remote has
         //             // closed
-        //             Ok(0) => return,
+        //             Ok(0) => {
+        //                 println!(
+        //                     "Reader read done. The value in the buffer is: {:?}",
+        //                     String::from_utf8_lossy(&mut buf[0..40])
+        //                 );
+        //                 return;
+        //             }
         //             Ok(n) => {
         //                 // Copy the data back to socket
+        //                 println!(
+        //                     "Server GOT from reader: {:?}",
+        //                     String::from_utf8_lossy(&buf[..n])
+        //                 );
         //                 if socket.write_all(&buf[..n]).await.is_err() {
         //                     // Unexpected socket error. There isn't much we can
         //                     // do here so just stop processing.
+        //                     eprintln!("Fail to echo value");
         //                     return;
         //                 }
         //             }
         //             Err(_) => {
         //                 // Unexpected socket error. There isn't much we can do
         //                 // here so just stop processing.
+        //                 eprintln!("Fail to echo value");
         //                 return;
         //             }
         //         }
